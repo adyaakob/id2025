8f831e34ad9fa3c4cf76df0457ba2c05b4d573c0
adyaakob <andylie2004@gmail.com>
2024-11-17 00:44:13 +0800

    Configure for GitHub Pages deployment

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  basePath: '/48-VDG-Product-Presentation',
+  images: {
+    unoptimized: true,
+  },
+}
+
+module.exports = nextConfig
