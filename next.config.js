96e72ea54f2715f94c8535ba8240e4d425816282
adyaakob <andylie2004@gmail.com>
2024-11-17 01:17:17 +0800

    Update deployment configuration

---
+++ next.config.js
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  basePath: '/id2025',
+  images: {
+    unoptimized: true,
+  },
+  trailingSlash: true,
+}
+
+module.exports = nextConfig
