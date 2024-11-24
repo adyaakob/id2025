1dcc5e476586973860439ec94f633c91f869b3ad
adyaakob <andylie2004@gmail.com>
2024-11-24 22:53:38 +0800

    chore: configure Next.js for GitHub Pages deployment

--- next.config.js
+++ next.config.js
@@ -1,12 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   images: {
     domains: ['unpkg.com'],
     unoptimized: true
   },
   typescript: {
     ignoreBuildErrors: true
-  }
+  },
+  basePath: '/id2025',
+  assetPrefix: '/id2025'
 }
 
 module.exports = nextConfig
